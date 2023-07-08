29cc29cc9a23fde42da03c3d383cfccc91ae4d1e
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-07-08 15:46:32 +0100

    update home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../Components/NavBar";
 import style from "./style.module.css"
 
@@ -8,32 +8,49 @@ import Footer from "../../Components/Footer";
 
 
 export default function Home(){
+    const user = localStorage.getItem("type")
+
     return(
         <>
             <NavBar></NavBar>
-            <div className={style.apresentation}>
-                <div className={style.aprentationImg}>
-                    <img src={coolStars} height="100%"></img>
-                </div>
-                <div className={style.aprentationText}>
-                    <p>Descubra o que os outros estão<br/>
-                    a falar sobre os seus<br/>
-                    <span className={style.aprentationText_span1}>estabelecimentos</span> favoritos:<br/>
-                    compartilhe as suas <span className={style.aprentationText_span2}>experiências</span> e avalie os<br/>
-                    lugares que você já visitou!<br/></p>
-                    <a href="/avaliador" className={style.cadastroButton}>Cadastre-se</a>
-                </div>
-            </div>
+            {
+                localStorage.getItem("user") ? (
+                    //Se o utilizador estiver autenticado vamos a apresentar uma página especial para cada 
+                    //Primeiro precisamos de diferenciar o utilizador e um estabelecimento 
+                    
+                    <div>
 
-            <div className={style.apresentationForEstabelishments}>
-                <div className={style.aprentationTextForEstabelishments}>
-                    Junte-se à nossa comunidade e crie já a sua conta para deixar<br/>
-                    o seu estabelecimento aberto ao feedback dos seus clientes.
-                </div>
-                <div>
-                    <a href="/estabelecimento" className={style.cadastroButton}>Cadastre-se</a>
-                </div>
-            </div>
+                    </div>
+                ) : (
+                    <div>
+                        <div className={style.apresentation}>
+                        <div className={style.aprentationImg}>
+                            <img src={coolStars} height="100%"></img>
+                        </div>
+                        <div className={style.aprentationText}>
+                            <p>Descubra o que os outros estão<br/>
+                            a falar sobre os seus<br/>
+                            <span className={style.aprentationText_span1}>estabelecimentos</span> favoritos:<br/>
+                            compartilhe as suas <span className={style.aprentationText_span2}>experiências</span> e avalie os<br/>
+                            lugares que você já visitou!<br/></p>
+                            <a href="/avaliador" className={style.cadastroButton}>Cadastre-se</a>
+                        </div>
+                        </div>
+
+                        <div className={style.apresentationForEstabelishments}>
+                            <div className={style.aprentationTextForEstabelishments}>
+                                Junte-se à nossa comunidade e crie já a sua conta para deixar<br/>
+                                o seu estabelecimento aberto ao feedback dos seus clientes.
+                            </div>
+                            <div>
+                                <a href="/estabelecimento" className={style.cadastroButton}>Cadastre-se</a>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+            
 
             <Footer></Footer>
         </>
